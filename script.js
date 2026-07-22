17714a00d49e5f94c5027a4d3289bdb4d0318a37
zona747474-ai <zona747474@gmail.com>
2026-07-22 22:43:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIG & CONSTANTS
 // ==========================================
 // GANTI URL DI BAWAH INI DENGAN WEB APP URL GOOGLE APPS SCRIPT KAMU
-const API_URL = "URL_WEB_APP_APPS_SCRIPT_KAMU_DI_SINI";
+const API_URL = "https://script.google.com/macros/s/AKfycbyw8qSPlnsIZTOAndF26IcJqexOSULCEUmd4P6xSz5_Gpk1MtPoOnCm4pBXLdrLY2HG/exec";
 
 const STORAGE_KEY = "moneytrack_transactions_v2";
 const BUDGET_KEY = "moneytrack_monthly_budget";
